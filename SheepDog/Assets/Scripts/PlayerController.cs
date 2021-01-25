5e24c7cad4047af246bbf1a55bad0a357746b699
ArthurGICQUEL <arthurgcl@orange.fr>
2021-01-25 11:58:43 +0100

    Player movement 1

---
+++ SheepDog/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    float diagonalSpeed;
+    Vector2 movement;
+    void Start()
+    {
+        diagonalSpeed = speed / 1.41f;
+    }
+    void Update()
+    {
+        Movement();
+    }
+    void Movement()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        if (inputX != 0 && inputY != 0) movement = new Vector2(diagonalSpeed * inputX, diagonalSpeed * inputY);
+        else movement = new Vector2(speed * inputX, speed * inputY);
+
+        GetComponent<Rigidbody2D>().velocity = movement;
+    }
+}
